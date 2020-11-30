910685f5d715f69012627189a12f5f8fbe2edd5a
Sergio Abascià <Sergio05Rule@MBPdiAbiaSergio.homenet.telecomitalia.it>
2020-11-30 15:24:48 +0100

    Created Admin List Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useDispatch, useSelector} from 'react-redux'
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { signout } from './actions/userActions';
+import AdminRoute from './components/AdminRoute';
 import PrivateRoute from './components/PrivateRoute';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
@@ -9,6 +10,7 @@ import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import OrderScreen from './screens/OrderScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import ProductListScreen from './screens/ProductListScreen';
 import ProductScreen from './screens/ProductScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
@@ -99,6 +101,7 @@ function App() {
                     <Route path="/order/:id" component={OrderScreen}></Route>
                     <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
                     <PrivateRoute path="/profile" component={ProfileScreen}></PrivateRoute>
+                    <AdminRoute path="/productlist" component={ProductListScreen}></AdminRoute>
                     <Route path="/" component={HomeScreen} exact></Route>
                 </main>
                 <footer className="contacts">
