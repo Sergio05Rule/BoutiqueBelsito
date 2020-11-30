6f6a33b07dcde94247a8a6bb9b43de8fc13cbf0b
Sergio Abascià <Sergio05Rule@MacBook-Pro-di-Abascia-Sergio.local>
2020-11-30 16:48:53 +0100

    Implemented Admin Build Product Edit Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import OrderScreen from './screens/OrderScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductScreen from './screens/ProductScreen';
 import ProfileScreen from './screens/ProfileScreen';
@@ -93,6 +94,7 @@ function App() {
                 <main>
                     <Route path="/cart/:id?" component={CartScreen}></Route>
                     <Route path="/product/:id" component={ProductScreen} exact></Route>
+                    <Route path="/product/:id/edit" component={ProductEditScreen} exact></Route>
                     <Route path="/signin" component={SigninScreen}></Route>
                     <Route path="/register" component={RegisterScreen}></Route>
                     <Route path="/shipping" component={ShippingAddressScreen}></Route>
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -28,10 +28,7 @@ const {
     }
   };
   
-  export const productDetailsReducer = (
-    state = { product: {}, loading: true },
-    action
-  ) => {
+  export const productDetailsReducer = (state = { loading: true }, action) => {
     switch (action.type) {
       case PRODUCT_DETAILS_REQUEST:
         return { loading: true };
