4d4c75874f4e015bcb15b79866e97f0dd5b05e1d
Sergio Abascià <Sergio05Rule@MacBook-Pro-di-Abascia-Sergio.local>
2020-11-30 17:06:32 +0100

    Implemented Admin Build Update Product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -9,6 +9,10 @@ const {
     PRODUCT_CREATE_SUCCESS,
     PRODUCT_CREATE_FAIL,
     PRODUCT_CREATE_RESET,
+    PRODUCT_UPDATE_REQUEST,
+    PRODUCT_UPDATE_SUCCESS,
+    PRODUCT_UPDATE_FAIL,
+    PRODUCT_UPDATE_RESET,
   } = require('../constants/productConstants');
   
   // funcion that accept a state, action in input
@@ -50,6 +54,21 @@ const {
       case PRODUCT_CREATE_FAIL:
         return { loading: false, error: action.payload };
       case PRODUCT_CREATE_RESET:
+        return {};
+      default:
+        return state;
+    }
+  };
+
+  export const productUpdateReducer = (state = {}, action) => {
+    switch (action.type) {
+      case PRODUCT_UPDATE_REQUEST:
+        return { loading: true };
+      case PRODUCT_UPDATE_SUCCESS:
+        return { loading: false, success: true };
+      case PRODUCT_UPDATE_FAIL:
+        return { loading: false, error: action.payload };
+      case PRODUCT_UPDATE_RESET:
         return {};
       default:
         return state;
