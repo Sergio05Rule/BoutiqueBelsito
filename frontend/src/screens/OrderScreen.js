1e28f22de8823e63394856d97ed7c3b7363d9dbd
Sergio Abascià <Sergio05Rule@MBPdiAbiaSergio.homenet.telecomitalia.it>
2020-12-15 15:09:21 +0100

    Implemented OrderScreenStyle

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,15 +1,20 @@
-import Axios from 'axios';
-import { PayPalButton } from 'react-paypal-button-v2';
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { deliverOrder, detailsOrder, payOrder } from '../actions/orderActions';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
+import Axios from "axios";
+import { PayPalButton } from "react-paypal-button-v2";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { deliverOrder, detailsOrder, payOrder } from "../actions/orderActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 import {
   ORDER_DELIVER_RESET,
   ORDER_PAY_RESET,
-} from '../constants/orderConstants';
+} from "../constants/orderConstants";
+import { Row, Col, Dropdown, Container } from "react-bootstrap/";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
 
 export default function OrderScreen(props) {
   const orderId = props.match.params.id;
@@ -34,33 +39,33 @@ export default function OrderScreen(props) {
   const dispatch = useDispatch();
   useEffect(() => {
     const addPayPalScript = async () => {
-    const { data } = await Axios.get('/api/config/paypal');
-    const script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = `https://www.paypal.com/sdk/js?client-id=${data}`;
-    script.async = true;
-    script.onload = () => {
+      const { data } = await Axios.get("/api/config/paypal");
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = `https://www.paypal.com/sdk/js?client-id=${data}`;
+      script.async = true;
+      script.onload = () => {
         setSdkReady(true);
-    };
-    document.body.appendChild(script);
+      };
+      document.body.appendChild(script);
     };
     if (
       !order ||
       successPay ||
       successDeliver ||
       (order && order._id !== orderId)
-    ) {    
+    ) {
       dispatch({ type: ORDER_PAY_RESET });
       dispatch({ type: ORDER_DELIVER_RESET });
       dispatch(detailsOrder(orderId));
     } else {
-    if (!order.isPaid) {
+      if (!order.isPaid) {
         if (!window.paypal) {
-        addPayPalScript();
+          addPayPalScript();
         } else {
-        setSdkReady(true);
+          setSdkReady(true);
         }
-    }
+      }
     }
   }, [dispatch, order, orderId, sdkReady, successPay, successDeliver]);
 
@@ -71,155 +76,154 @@ export default function OrderScreen(props) {
   const deliverHandler = () => {
     dispatch(deliverOrder(order._id));
   };
-  
+
   return loading ? (
     <LoadingBox></LoadingBox>
   ) : error ? (
     <MessageBox variant="danger">{error}</MessageBox>
   ) : (
-    <div>
-      <h1>Numer Ordine: {order._id}</h1>
-      <div className="row top">
-        <div className="col-2">
-          <ul>
-            <li>
-              <div className="card card-body">
-                <h2>Informazioni di spedizione</h2>
-                <p>
-                  <strong>Nome:</strong> {order.shippingAddress.fullName} <br />
-                  <strong>Indirizzo: </strong> {order.shippingAddress.address},
-                  {order.shippingAddress.city},{' '}
-                  {order.shippingAddress.postalCode},
-                  {order.shippingAddress.country}
-                </p>
-                {order.isDelivered ? (
+    <Container>
+    <Row>
+      <Col md={12}><h1>Numer Ordine: {order._id}</h1></Col>
+      </Row>
+      <Row>
+        <Col md={8}>
+          <Card>
+            <CardContent>
+              <Typography variant="h2" component="h2">
+                Informazioni di spedizione
+              </Typography>
+              <Typography variant="h5" component="h2">
+                <strong>Nome:</strong> {order.shippingAddress.fullName}
+              </Typography>
+
+              <Typography variant="h5" component="h2">
+                <strong>Indirizzo: </strong> {order.shippingAddress.address},
+                {order.shippingAddress.city}, {order.shippingAddress.postalCode}
+                ,{order.shippingAddress.country}
+              </Typography>
+              {order.isDelivered ? (
+                <Typography variant="h5" component="h2">
                   <MessageBox variant="success">
                     Spedito il {order.deliveredAt}
                   </MessageBox>
-                ) : (
+                </Typography>
+              ) : (
+                <Typography variant="h5" component="h2">
                   <MessageBox variant="danger">Non ancora spedito</MessageBox>
-                )}
-              </div>
-            </li>
-            <li>
-              <div className="card card-body">
-                <h2>Informazioni di pagamento</h2>
-                <p>
-                  <strong>Medoto di pagamento:</strong> {order.paymentMethod}
-                </p>
-                {order.isPaid ? (
+                </Typography>
+              )}
+            </CardContent>
+          </Card>
+
+          <Card className="mt-3">
+            <CardContent>
+              <Typography variant="h2" component="h2">
+                Informazioni di pagamento
+              </Typography>
+
+              <Typography variant="h5" component="h2">
+                <strong>Medoto di pagamento:</strong> {order.paymentMethod}
+              </Typography>
+              {order.isPaid ? (
+                <Typography variant="h5" component="h2">
                   <MessageBox variant="success">
                     Pagato il {order.paidAt}
                   </MessageBox>
-                ) : (
+                </Typography>
+              ) : (
+                <Typography variant="h5" component="h2">
                   <MessageBox variant="danger">Non pagato</MessageBox>
-                )}
-              </div>
-            </li>
-            <li>
-              <div className="card card-body">
-                <h2>Oggetti ordine</h2>
-                <ul>
-                  {order.orderItems.map((item) => (
-                    <li key={item.product}>
-                      <div className="row">
-                        <div>
-                          <img
-                            src={item.image}
-                            alt={item.name}
-                            className="small"
-                          ></img>
-                        </div>
-                        <div className="min-30">
-                          <Link to={`/product/${item.product}`}>
-                            {item.name}
-                          </Link>
-                        </div>
-                        <div>
-                         Taglia: {item.size} 
-                        </div>
-                        <div>
-                          {item.qty} x {item.price}€ = {item.qty * item.price}€
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className="col-1">
-          <div className="card card-body">
-            <ul>
-              <li>
-                <h2>Sommario ordine</h2>
-              </li>
-              <li>
-                <div className="row">
-                  <div>Oggetto/i</div>
-                  <div>{order.itemsPrice.toFixed(2)}€</div>
-                </div>
-              </li>
-              <li>
-                <div className="row">
-                  <div>Spedizione</div>
-                  <div>{order.shippingPrice.toFixed(2)}€</div>
-                </div>
-              </li>
-              <li>
-                <div className="row">
-                  <div>Tasse:</div>
-                  <div>{order.taxPrice.toFixed(2)}€</div>
-                </div>
-              </li>
-              <li>
-                <div className="row">
-                  <div>
-                    <strong>Totale oridine</strong>
-                  </div>
-                  <div>
-                    <strong>{order.totalPrice.toFixed(2)}€</strong>
-                  </div>
-                </div>
-              </li>
-               {!order.isPaid && ( // if true render this JSX element
-                <li>
+                </Typography>
+              )}
+            </CardContent>
+          </Card>
+          <Card className="mt-3">
+            <CardContent>
+              <Typography variant="h2" component="h2">
+                Oggetti ordine
+              </Typography>
+              {order.orderItems.map((item) => (
+                <Row className="mt-3">
+                  <Col md={3}>
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className="small"
+                    ></img>
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={3}>Taglia: {item.size}</Col>
+                  <Col md={3}>
+                    {item.qty} x {item.price}€ = {item.qty * item.price}€
+                  </Col>
+                </Row>
+              ))}
+            </CardContent>
+          </Card>
+        </Col>
+
+        <Col md={4}>
+          <Card className="mt-3">
+            <CardContent>  
+            <Typography variant="h2" component="h2">
+                Sommario ordine
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    Oggetto/i: {order.itemsPrice.toFixed(2)}€
+                    </Typography>
+                  <Typography gutterBottom variant="h5" component="h2">
+                  Spedizione: {order.shippingPrice.toFixed(2)}€
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    Tasse: {order.taxPrice.toFixed(2)}€
+                    </Typography>
+                  <Typography gutterBottom variant="h5" component="h2">
+                     <strong>Totale oridine: {order.totalPrice.toFixed(2)}€</strong>
+                    </Typography>
+              {!order.isPaid && ( // if true render this JSX element
+                <>
                   {!sdkReady ? (
                     <LoadingBox></LoadingBox>
-                  ) : ( <>
-                    {errorPay && (
-                      <MessageBox variant="danger">{errorPay}</MessageBox>
-                    )}
-                    {loadingPay && <LoadingBox></LoadingBox>}
-
-                    <PayPalButton
-                      amount= {order.totalPrice}
-                      onSuccess={successPaymentHandler}
-                    ></PayPalButton>
-                  </>
+                  ) : (
+                    <>
+                      {errorPay && (
+                        <MessageBox variant="danger">{errorPay}</MessageBox>
+                      )}
+                      {loadingPay && <LoadingBox></LoadingBox>}
+                      <Typography variant="h2" component="h2">
+                      <PayPalButton
+                        amount={order.totalPrice}
+                        onSuccess={successPaymentHandler}
+                      ></PayPalButton>
+                                          </Typography>
+                    </>
                   )}
-                </li>
+                </>
               )}
               {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
-                <li>
+                <>
                   {loadingDeliver && <LoadingBox></LoadingBox>}
                   {errorDeliver && (
                     <MessageBox variant="danger">{errorDeliver}</MessageBox>
                   )}
-                  <button
+                  <Button
                     type="button"
-                    className="primary block"
+                    variant="contained"
+                color="primary"
+                className="w-100"
                     onClick={deliverHandler}
                   >
                     Spegna come spedito
-                  </button>
-                </li>
+                  </Button>
+                </>
               )}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+            </CardContent>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
